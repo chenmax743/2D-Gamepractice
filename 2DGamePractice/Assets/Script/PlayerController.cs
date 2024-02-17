fa93ff47a487e7f24efbf116e8b5252532b6ff29
chenmax743 <chenmax38@gmail.com>
2024-02-17 14:56:26 +0800

    RN004
    增加角色動畫(移動)

---
+++ 2DGamePractice/Assets/Script/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Flip();
+    }
+
+    void Flip()
+    {
+        bool playerHasXAxisSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        anim.SetBool("Run", playerHasXAxisSpeed);
+        if(playerHasXAxisSpeed)
+        {
+            if(rb.velocity.x>0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            
+            if(rb.velocity.x<-0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+    }
+
+    void Move()
+    {
+        float movedirection = Input.GetAxis("Horizontal");
+        Vector2 player = new Vector2(movedirection * Speed, rb.velocity.y);
+        rb.velocity = player;
+        bool playerHasXAxisSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        anim.SetBool("Run", playerHasXAxisSpeed);
+    }
+}
